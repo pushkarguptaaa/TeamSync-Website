4b45185edb42d08fa0cc1aff20ac78a46d393946
Pushkar Gupta <pushkardps8@gmail.com>
2024-11-20 16:42:29 +0530

    created hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from "@/components/Banner";
+import { Hero } from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return <>
     <Banner />
     <Navbar />
+    <Hero />
   </>
 }
